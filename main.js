9b957ca53857dac007c7f04694056a1d525997d2
Dario Kovačić <dariok.dev@gmail.com>
2021-02-01 13:16:08 +0100

    completed exercise

--- main.js
+++ main.js
@@ -10,7 +10,24 @@ for (let i = 1; i <= 5; i++) {
   const newImage = document.createElement('img');
   let path = `images/pic${i}.jpg`;
   newImage.setAttribute('src', path);
-  thumbBar.appendChild(newImage);  
+  newImage.onclick = function () {
+    displayedImage.setAttribute('src', path);
+  };
+
+  thumbBar.appendChild(newImage);
+}
+
+btn.onclick = function () {
+  const className = this.getAttribute('class');
+  if (className === 'dark') {
+    this.setAttribute('class', 'light');
+    this.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    this.setAttribute('class', 'dark');
+    this.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
 }
 
 
