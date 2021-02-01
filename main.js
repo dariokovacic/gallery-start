afd0c54d92cffa19b417ba5d7bc1658b41b1e5ad
Dario Kovačić <dariok.dev@gmail.com>
2021-02-01 12:45:09 +0100

    add images

--- main.js
+++ main.js
@@ -6,8 +6,13 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 1; i <= 5; i++) {
+  const newImage = document.createElement('img');
+  let path = `images/pic${i}.jpg`;
+  newImage.setAttribute('src', path);
+  thumbBar.appendChild(newImage);  
+}
+
+
 
 /* Wiring up the Darken/Lighten button */
